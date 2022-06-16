03aa818c8d234c810895be14e5ed0ecfaeb60609
Mario Gallaun <mario.gallaun@gmx.at>
2022-06-16 14:22:44 +0200

    Documentation for LEDControl.cpp

--- lib/LEDControl/LEDControl.cpp
+++ lib/LEDControl/LEDControl.cpp
@@ -1,6 +1,10 @@
 #include <utility/wifi_drv.h>
 #include "LedControl.h" 
 
+/**
+ * Class to control the RBG LED on the ArduinoUno WiFi
+ * The LED is connected to the WiFI module so we need to communication with the WiFi module to controll the LED
+ **/ 
 LEDControl::LEDControl() { 
   onColor[0] = 0;
   onColor[1] = 0;
@@ -11,6 +15,9 @@ LEDControl::LEDControl() {
   offColor[2] = 0; 
 }
 
+/**
+ * Configure OutPuts via WiFiDrv/WiFi module for the RBG Led
+ **/ 
 void LEDControl::init() {
   WiFiDrv::pinMode(25, OUTPUT);  //RED
   WiFiDrv::pinMode(26, OUTPUT);  //GREEN
@@ -20,24 +27,36 @@ void LEDControl::init() {
   WiFiDrv::analogWrite(27, 0);  // for configurable brightness  
 }
 
+/**
+ * Set the "ON" Color of the LED
+ **/ 
 void LEDControl::setOnColor(int red, int green, int blue) {
     onColor[0] = red;
     onColor[1] = green;
     onColor[2] = blue;
 }
 
+/**
+ * Set the "OFF" Color of the LED
+ **/ 
 void LEDControl::setOffColor(int red, int green, int blue) {
     offColor[0] = red;
     offColor[1] = green;
     offColor[2] = blue;
 }
 
+/**
+ * Enable the LED with set colors (setOnColor)
+ **/ 
 void LEDControl::enable() {
   WiFiDrv::analogWrite(25, onColor[0]);
   WiFiDrv::analogWrite(26, onColor[1]);
   WiFiDrv::analogWrite(27, onColor[2]);
 }
 
+/**
+ * Disable the LED with set colors (setOffColor)
+ **/ 
 void LEDControl::disable() {
   WiFiDrv::analogWrite(25, offColor[0]);
   WiFiDrv::analogWrite(26, offColor[1]);
